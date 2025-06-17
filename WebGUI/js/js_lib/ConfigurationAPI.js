51e4a9027c777d8c9d289c4305775fbdc0922f38
Ali Mohammed <alim@fnal.gov>
2025-06-17 11:40:46 -0500

    fixed flipped bitmaps core issue

--- WebGUI/js/js_lib/ConfigurationAPI.js
+++ WebGUI/js/js_lib/ConfigurationAPI.js
@@ -3802,10 +3802,11 @@ ConfigurationAPI.bitMapDialog = function(tableName,UIDName,fieldNameRaw,fieldNam
 								" @ (row,col) = (" +
 								convertedRC[0] + "," + convertedRC[0] + ")");
 					color = localConvertValueToRGBA(bmpData[r][c]);
-					bmpDataImage.data[(r*cols + c)*4+0]=color[0];
-					bmpDataImage.data[(r*cols + c)*4+1]=color[1];
-					bmpDataImage.data[(r*cols + c)*4+2]=color[2];
-					bmpDataImage.data[(r*cols + c)*4+3]=color[3];
+					let flippedRow = rows - 1 - r;
+					bmpDataImage.data[(flippedRow*cols + c)*4+0]=color[0];
+					bmpDataImage.data[(flippedRow*cols + c)*4+1]=color[1];
+					bmpDataImage.data[(flippedRow*cols + c)*4+2]=color[2];
+					bmpDataImage.data[(flippedRow*cols + c)*4+3]=color[3];
 				}
 				catch(err)
 				{noErrors = false;} //ignore errors
