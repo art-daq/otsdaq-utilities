498a9d934ab547775f1f0c5afd6ff875ec45762b
RYAN RIVERA <rrivera@FNAL.GOV>
2025-06-20 17:42:15 -0500

    Better warnings when on a branch; better tabbing in web Debug popups for Errors

--- WebGUI/js/DesktopContent.js
+++ WebGUI/js/DesktopContent.js
@@ -1466,8 +1466,7 @@ DesktopContent.XMLHttpRequest = function(requestURL, data, returnHandler,
 //				Debug.log("Status=0. Likely this means a window was closed, or the server crashed, in the middle of a request. " +
 //						"\n(It also could mean 'potential security risk' like a cross-domain request) ",Debug.HIGH_PRIORITY);
 				errStr = "Request was interrupted. " +
-						"Likely this means the server crashed or connection was lost (or the desktop window making the request was closed), " +
-						" in the middle of a request.";
+						"Likely this means the server crashed or connection was lost (or the desktop window making the request was closed; or did your ssh tunnel close?).";
 				//return;
 			}
 			else //bad address response
--- tools/UpdateOTS.sh
+++ tools/UpdateOTS.sh
@@ -341,6 +341,12 @@ if [ "$1"  == "--warn" ]; then #warn should be quiet unless (on stderr) there ar
 		# else
 		# 	echo "Working tree is clean."
 		fi
+		branch="$(git rev-parse --abbrev-ref HEAD)"
+		if [ "$branch" != "main" ] && [ "$branch" != "develop" ]; then
+			echo -e  " ===|>  WARNING!!! Found unmerged BRANCH in repository ${OTS_SOURCE}/../otsdaq-mu2e-config ==> ${branch}" >&2 #take stderr for warn result
+		# else
+		# 	echo "You are on main or develop"
+		fi
 		cd -
 	fi
 	#check daq-operations outside of srcs
@@ -350,6 +356,12 @@ if [ "$1"  == "--warn" ]; then #warn should be quiet unless (on stderr) there ar
 			echo -e  " ===|>  WARNING!!! Found uncommitted changes in repository ${OTS_SOURCE}/../daq-operations" >&2 #take stderr for warn result
 		# else
 		# 	echo "Working tree is clean."
+		fi
+		branch="$(git rev-parse --abbrev-ref HEAD)"
+		if [ "$branch" != "main" ] && [ "$branch" != "develop" ]; then
+			echo -e  " ===|>  WARNING!!! Found unmerged BRANCH in repository ${OTS_SOURCE}/../daq-operations ==> ${branch}" >&2 #take stderr for warn result
+		# else
+		# 	echo "You are on main or develop"
 		fi
 		cd -
 	fi
@@ -502,7 +514,9 @@ echo -e "UpdateOTS.sh:${LINENO}  \t =================="
 echo -e "UpdateOTS.sh:${LINENO}  \t Git comment '$GIT_COMMENT'"
 echo -e "UpdateOTS.sh:${LINENO}  \t Status will be logged here: $CHECKIN_LOG_PATH"
 
-echo "List of srcs repos:" > $CURRENT_AWESOME_BASE/list_of_repos.txt
+if [ $SHARE_ONLY = 1 ]; then
+	echo "List of srcs repos:" > $CURRENT_AWESOME_BASE/list_of_repos.txt
+fi
 
 echo
 echo -e "UpdateOTS.sh:${LINENO}  \t =================="
@@ -541,6 +555,12 @@ for p in ${REPO_DIR[@]}; do
 			echo -e  " ===|>  WARNING!!! Found uncommitted changes in repository $p" >&2 #take stderr for warn result
 		# else
 		# 	echo "Working tree is clean."
+		fi
+		branch="$(git rev-parse --abbrev-ref HEAD)"
+		if [ "$branch" != "main" ] && [ "$branch" != "develop" ]; then
+			echo -e  " ===|>  WARNING!!! Found unmerged BRANCH in repository $p ==> ${branch}" >&2 #take stderr for warn result
+		# else
+		# 	echo "You are on main or develop"
 		fi
 	else
 		echo -e "UpdateOTS.sh:${LINENO}  \t Pulling updates from $p"
@@ -593,8 +613,8 @@ if [[ "x$GIT_COMMENT" == "x" && $FETCH_ONLY = 0 ]]; then
 	timeout 1 rm $OTS_SOURCE/../reset_ots_tutorial.sh &>/dev/null 2>&1 #hide output (could hang if weird permission, so use timeout)
 	# echo -e "UpdateOTS.sh:${LINENO}  \t cp $LOC_OTS_DIR/../otsdaq_demo/tools/reset_ots_tutorial.sh $LOC_OTS_DIR/../../reset_ots_tutorial.sh"
 	#cp $LOC_OTS_DIR/../otsdaq_demo/tools/reset_ots_tutorial.sh $LOC_OTS_DIR/../../reset_ots_tutorial.sh
-	wget -T 5 https://github.com/art-daq/otsdaq_demo/raw/develop/tools/reset_ots_tutorial.sh -P $OTS_SOURCE/../ --no-check-certificate	 &>/dev/null 2>&1
-	chmod 644 $OTS_SOURCE/../reset_ots_tutorial.sh
+	wget -T 5 -O $OTS_SOURCE/../reset_ots_tutorial.sh https://github.com/art-daq/otsdaq_demo/raw/develop/tools/reset_ots_tutorial.sh -P $OTS_SOURCE/../ --no-check-certificate	 &>/dev/null 2>&1
+	chmod 644 $OTS_SOURCE/../reset_ots_tutorial.sh #for safety, prevent accidental execution by users
 
 	rm $OTS_SOURCE/../reset_ots_artdaq_tutorial.sh &>/dev/null 2>&1 #hide output
 	#now there is only one reset_tutorial script (that includes the artdaq tutorial), so cleanup old systems and do not download script
