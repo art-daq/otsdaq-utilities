086c1425b981237d4dfa9dbf4e7dacdda334af02
Ali Mohammed <alim@fnal.gov>
2025-06-20 15:13:06 -0500

    whitespace fix

--- WebGUI/js/js_lib/ConfigurationAPI.js
+++ WebGUI/js/js_lib/ConfigurationAPI.js
@@ -3803,8 +3803,8 @@ ConfigurationAPI.bitMapDialog = function(tableName,UIDName,fieldName,bitMapParam
 								convertedRC[0] + "," + convertedRC[0] + ")");
 					color = localConvertValueToRGBA(bmpData[r][c]);
 					bmpDataImage.data[(r*cols + c)*4+0]=color[0];
- 					bmpDataImage.data[(r*cols + c)*4+1]=color[1];
- 					bmpDataImage.data[(r*cols + c)*4+2]=color[2];
+					bmpDataImage.data[(r*cols + c)*4+1]=color[1];
+					bmpDataImage.data[(r*cols + c)*4+2]=color[2];
 					bmpDataImage.data[(r*cols + c)*4+3]=color[3];
 				}
 				catch(err)
