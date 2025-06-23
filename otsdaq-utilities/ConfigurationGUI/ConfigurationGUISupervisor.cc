bbd12b54d924a6e8044fd0452e7aa9809d5f496b
RYAN RIVERA <rrivera@FNAL.GOV>
2025-06-23 10:05:32 -0500

    Clangify

--- otsdaq-utilities/ConfigurationGUI/ConfigurationGUISupervisor.cc
+++ otsdaq-utilities/ConfigurationGUI/ConfigurationGUISupervisor.cc
@@ -7871,39 +7871,48 @@ void ConfigurationGUISupervisor::handleGetArtdaqNodeRecordsXML(
 			paramIndex = 3;  // start at 3 after subsystem parameter
 			if(artdaqNode.second.size() > paramIndex)
 			{
-				__SUP_COUTT__ << "\t\t\t" << "-multinode: " << artdaqNode.second[paramIndex] << __E__;
+				__SUP_COUTT__ << "\t\t\t"
+				              << "-multinode: " << artdaqNode.second[paramIndex] << __E__;
 				xmlOut.addTextElementToParent(
 				    typeString + "-multinode", artdaqNode.second[paramIndex++], nodeEl);
 			}
 			if(artdaqNode.second.size() > paramIndex)
 			{
-				__SUP_COUTT__ << "\t\t\t" << "-nodefixedwidth: " << artdaqNode.second[paramIndex] << __E__;
+				__SUP_COUTT__ << "\t\t\t"
+				              << "-nodefixedwidth: " << artdaqNode.second[paramIndex]
+				              << __E__;
 				xmlOut.addTextElementToParent(typeString + "-nodefixedwidth",
 				                              artdaqNode.second[paramIndex++],
 				                              nodeEl);
 			}
 			if(artdaqNode.second.size() > paramIndex)
 			{
-				__SUP_COUTT__ << "\t\t\t" << "-hostarray: " << artdaqNode.second[paramIndex] << __E__;
+				__SUP_COUTT__ << "\t\t\t"
+				              << "-hostarray: " << artdaqNode.second[paramIndex] << __E__;
 				xmlOut.addTextElementToParent(
 				    typeString + "-hostarray", artdaqNode.second[paramIndex++], nodeEl);
 			}
 			if(artdaqNode.second.size() > paramIndex)
 			{
-				__SUP_COUTT__ << "\t\t\t" << "-hostfixedwidth: " << artdaqNode.second[paramIndex] << __E__;
+				__SUP_COUTT__ << "\t\t\t"
+				              << "-hostfixedwidth: " << artdaqNode.second[paramIndex]
+				              << __E__;
 				xmlOut.addTextElementToParent(typeString + "-hostfixedwidth",
 				                              artdaqNode.second[paramIndex++],
 				                              nodeEl);
 			}
 
 			paramIndex = 0;  // return to starting parameter
-			__SUP_COUTT__ << "\t\t\t" << "-status: " << artdaqNode.second[paramIndex] << __E__;
+			__SUP_COUTT__ << "\t\t\t"
+			              << "-status: " << artdaqNode.second[paramIndex] << __E__;
 			xmlOut.addTextElementToParent(
 			    typeString + "-status", artdaqNode.second[paramIndex++], parentEl);
-			__SUP_COUTT__ << "\t\t\t" << "-hostname: " << artdaqNode.second[paramIndex] << __E__;
+			__SUP_COUTT__ << "\t\t\t"
+			              << "-hostname: " << artdaqNode.second[paramIndex] << __E__;
 			xmlOut.addTextElementToParent(
 			    typeString + "-hostname", artdaqNode.second[paramIndex++], parentEl);
-			__SUP_COUTT__ << "\t\t\t" << "-subsystem: " << artdaqNode.second[paramIndex] << __E__;
+			__SUP_COUTT__ << "\t\t\t"
+			              << "-subsystem: " << artdaqNode.second[paramIndex] << __E__;
 			xmlOut.addTextElementToParent(
 			    typeString + "-subsystem", artdaqNode.second[paramIndex], parentEl);
 		}
@@ -8058,8 +8067,8 @@ void ConfigurationGUISupervisor::handleLoadArtdaqNodeLayoutXML(
 	        : contextGroupKey;
 
 	std::stringstream layoutPath;
-	layoutPath << ARTDAQTableBase::ARTDAQ_CONFIG_LAYOUTS_PATH << finalContextGroupName << "_"
-	           << finalContextGroupKey << ".dat";
+	layoutPath << ARTDAQTableBase::ARTDAQ_CONFIG_LAYOUTS_PATH << finalContextGroupName
+	           << "_" << finalContextGroupKey << ".dat";
 	__SUP_COUTV__(layoutPath.str());
 
 	FILE* fp = fopen(layoutPath.str().c_str(), "r");
@@ -8141,8 +8150,8 @@ void ConfigurationGUISupervisor::handleSaveArtdaqNodeLayoutXML(
 	__SUP_COUTV__(layoutString);
 
 	std::stringstream layoutPath;
-	layoutPath << ARTDAQTableBase::ARTDAQ_CONFIG_LAYOUTS_PATH << finalContextGroupName << "_"
-	           << finalContextGroupKey << ".dat";
+	layoutPath << ARTDAQTableBase::ARTDAQ_CONFIG_LAYOUTS_PATH << finalContextGroupName
+	           << "_" << finalContextGroupKey << ".dat";
 	__SUP_COUTV__(layoutPath.str());
 
 	std::vector<std::string> fields = StringMacros::getVectorFromString(layoutString);
@@ -8708,8 +8717,8 @@ void ConfigurationGUISupervisor::handleTableDiff(HttpXmlDocument&        xmlOut,
 ///		test activation of context group
 void ConfigurationGUISupervisor::testXDAQContext()
 {
-	ConfigurationManagerRW cfgMgrInst("ExampleUser");
-	ConfigurationManagerRW* cfgMgr =& cfgMgrInst;
+	ConfigurationManagerRW  cfgMgrInst("ExampleUser");
+	ConfigurationManagerRW* cfgMgr = &cfgMgrInst;
 	cfgMgr->testXDAQContext();
 	return;
 
