dead9715b6a29ad01b35b7f2b4d47e79338aac36
Javier Campos <jcampos@fnal.gov>
2026-01-16 15:43:33 -0600

    Fixing typo and defining objHandler as local var

--- WebGUI/js/visualizers_lib/ViewerRoot.js
+++ WebGUI/js/visualizers_lib/ViewerRoot.js
@@ -766,13 +766,13 @@ ViewerRoot.rootReq = function(rootPath,refreshIndex) {
 
 	if(refreshIndex === undefined) refreshIndex = -1;
 
-	objHanlder = [rootPath,refreshIndex];
+	var objHandlder = [rootPath,refreshIndex];
 
 	Debug.log("ViewerRoot.rootReq " + rootPath );
 	DesktopContent.XMLHttpRequest("Request?RequestType=getRoot",
 			"RootPath="+rootPath,
 			ViewerRoot.getRootDataHandler,
-			objHanlder /*reqParam*/,
+			objHandlder /*reqParam*/,
 			 0 /*progressHandler*/,
 			 0 /*callHandlerOnErr*/,
 			 refreshIndex<0?false:true /*doNoShowLoadingOverlay*/);
@@ -905,7 +905,7 @@ ViewerRoot.haltRefresh = function(rootName, debugLevel) {
 //=====================================================================================
 // ViewerRoot.getRootDataHandler ~~
 //	receives streamed root object from server and prepares it for js structures
-ViewerRoot.getRootDataHandler = function(req, objHanlder)
+ViewerRoot.getRootDataHandler = function(req, objHandlder)
 {
 
 	//Debug.log("ViewerRoot getRootDataHandler " + req.responseText );
@@ -916,7 +916,7 @@ ViewerRoot.getRootDataHandler = function(req, objHanlder)
 	//"my" + rootType + ViewerRoot.objIndex;// DesktopContent.getXMLValue(req,"path");// + ViewerRoot.objIndex;
 	//if(rootName.length > 20) rootName = "..." + rootName.substr(rootName.length-18);
 
-	const [rootPath,refreshIndex] = objHanlder;
+	const [rootPath,refreshIndex] = objHandlder;
 
 	var rootJSON = DesktopContent.getXMLValue(req,"rootJSON");
 
