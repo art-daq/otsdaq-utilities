1bd1111c018fe06f0bd325f602dfb1f27cda303c
RYAN RIVERA <rrivera@FNAL.GOV>
2026-02-20 12:08:52 -0600

    Realigned with github CI whitespace check

--- tools/ots_check_whitespace.sh
+++ tools/ots_check_whitespace.sh
@@ -14,30 +14,39 @@ fi
 excluded_dirs="WebGUI/js/js_lib WebGUI/js/visualizers_lib" #could space-separate excluded directories
 exclude_regex="^(nothingtoseehere|Data_.*|databases_.*"
 
-for dir in $excluded_dirs;do
-if [ -d $dir ]; then
-	exclude_regex="${exclude_regex}|$dir"
+#copied from otdsaq-utilities Check Git whitespace / Check Git whitespace on 20-Feb-2026
+for dir in \$excluded_dirs;do
+if [ -d \$dir ] || [ -f \$dir ]; then
+	exclude_regex="\${exclude_regex}|\$dir"
 fi
 done
-exclude_regex="(${exclude_regex}))"
-echo "Excluding files from checks via ${exclude_regex}"
-
-against=0
-if [ "x$against" == "x" ] || [ "x$against" == "x0" ]; then
-	against=$(git hash-object -t tree /dev/null)
+if [ -f .git_whitespace_exclude ];then
+while read entry;do
+	if [ "x\$entry" == "x" ]; then continue; fi
+	if [ -d \$entry ] || [ -f \$entry ]; then
+	exclude_regex="\${exclude_regex}|\$entry"
+	fi
+done < .git_whitespace_exclude
 fi
-echo "Checking for whitespace differences introduced since commit $against"
+exclude_regex="\${exclude_regex})"
+echo "Excluding files from checks via \${exclude_regex}"
+
+against=ab8055f50d452954d26e4e02ca60df40f98c9b8d
+if [ "x\$against" == "x" ] || [ "x\$against" == "x0" ]; then
+	against=\$(git hash-object -t tree /dev/null)
+fi
+echo "Checking for whitespace differences introduced since commit \$against"
 
 # Cross platform projects tend to avoid non-ASCII filenames; prevent
 # them from being added to the repository. We exploit the fact that the
 # printable range starts at the space character and ends with tilde.
-badchar=$(git diff --cached --name-only --diff-filter=A  $against | grep -vE "${exclude_regex}" |   LC_ALL=C tr -d 'A-Za-z0-9/\n_.@+-' | wc -c)
-if [ $badchar != 0 ]
+badchar=\$(git diff --cached --name-only --diff-filter=A  \$against | grep -vE "\${exclude_regex}" | \
+LC_ALL=C tr -d 'A-Za-z0-9/\n_ .@+-' | wc -c)
+if [ \$badchar != 0 ]
 then
 	echo "Check failed: cannot add a file name with non-ASCII or blank char."
 	exit 1
 fi
-
 
 names=$( git diff --cached --name-only --diff-filter=AM  $against | grep -vE "${exclude_regex}" )
 
