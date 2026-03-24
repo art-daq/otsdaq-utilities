8c8b07dcbcd70ce13baa058f82d5fde81a8c9426
RYAN RIVERA <rrivera@FNAL.GOV>
2026-03-24 18:49:44 -0500

    Format fix

--- test/ots_mm_udp_test.cpp
+++ test/ots_mm_udp_test.cpp
@@ -154,9 +154,8 @@ try
 	{
 		inputs.push_back(ots_mm_udp_interface::encodeURIComponent(argv[5 + i]));
 		std::string inputName = mm.getCommandInputName(fes[fe], commands[cmd], i);
-		__COUT_INFO__ << "\tInput #" << i << ": " << inputName << " = " << 
-					ots_mm_udp_interface::decodeURIComponent(inputs.back())
-		              << __E__;
+		__COUT_INFO__ << "\tInput #" << i << ": " << inputName << " = "
+		              << ots_mm_udp_interface::decodeURIComponent(inputs.back()) << __E__;
 	}
 
 	__COUT_INFO__ << "Running command with these expected outputs..." << __E__;
